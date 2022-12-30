b411839e84a9a5054dbd4609560e055b1673b0d9
Arthur Ming <howard5816632@gmail.com>
2022-12-30 09:20:01 +0800

    Initial Commit

---
+++ mediaFinal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mediaFinal
+//
+//  Created by 許允銘 on 2022/12/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
